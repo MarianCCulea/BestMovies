9021567aca1eeaad45ccb411914cef8a78af3a8d
Marian-Claudiu Culea <marianclaudiu.culea@gmail.com>
2021-06-01 15:21:40 +0200

    CleanUp
    Start

---
+++ BestMovies/Model/Mediator/DataLayer.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Components;
+using System.Net.Http.Headers;
+using System.Runtime.Serialization.Json;
+using System.Net.Http;
+using System.Text.Json;
+using Newtonsoft.Json;
+using Microsoft.AspNetCore.Authorization;
+using BestMovies.Pages;
+using BestMovies.Model.Domain;
+
+public interface IDataLayer
+{
+    IList<Movie> AllItems();
+    Movie ItemById(int id);
+}
+
+namespace BestMovies.Pages
+{
+    public class DataLayer : IDataLayer
+    { 
+
+        public IList<Movie> movies { get; set; }
+        public static DataLayer instance = null;
+        private static readonly object padlock = new object();
+        HttpClient client = new HttpClient();
+        public DataLayer()
+        {
+            client.DefaultRequestHeaders.Accept.Clear();
+            client.DefaultRequestHeaders.Accept.Add(
+               new MediaTypeWithQualityHeaderValue("application/vnd.github.v3+json"));
+            client.DefaultRequestHeaders.Add("User-Agent", ".NET Foundation Repository Reporter");
+        }
+
+        public static DataLayer Instance
+        {
+            get
+            {
+                lock (padlock)
+                {
+                    if (instance == null)
+                    {
+                        instance = new DataLayer();
+                    }
+                    return instance;
+                }
+            }
+        }
+
+        public IList<Movie> AllItems() => movies;
+        public Movie ItemById(int id) => movies.SingleOrDefault();
+        public Movie CloneItem(Movie source) => new Movie
+        {
+        };
+
+        /*
+        public async Task<string> SubmitChangesAsync(Movie item)
+        {
+            var serializer = new DataContractJsonSerializer(typeof(Movie));
+            //JavaScriptSerializer json_serializer = new JavaScriptSerializer();
+            string uri = "https://localhost:8080/items/" + item.id;
+            string jsonString;
+            jsonString = JsonConvert.SerializeObject(item);
+
+            HttpContent content = new StringContent(jsonString);
+            var streamTask = client.PutAsync(uri, content);
+            string result = await streamTask.Result.Content.ReadAsStringAsync();
+            if (result.Equals("true"))
+            {
+                movies = await RequestAllItemsAsync();
+                return "Success";
+            }
+            else
+            {
+                return "Fail";
+            }
+        }
+
+        */
+        public async Task<IList<Movie>> RequestAllItemsAsync()
+        {
+            string uri = "https://localhost:8080/items";
+            var streamTask = client.GetAsync(uri);
+            var stream = await streamTask.Result.Content.ReadAsStringAsync();
+            movies = JsonConvert.DeserializeObject<List<Movie>>(stream);
+            return movies;
+        }
+    }
+}
--- BestMovies/Startup.cs
+++ BestMovies/Startup.cs
@@ -1,4 +1,4 @@
-using BestMovies.Data;
+using BestMovies.Pages;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Components;
 using Microsoft.AspNetCore.Hosting;
@@ -28,7 +28,7 @@ namespace BestMovies
         {
             services.AddRazorPages();
             services.AddServerSideBlazor();
-            services.AddSingleton<WeatherForecastService>();
+            services.AddSingleton<IDataLayer, DataLayer>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
