8c023d4793e86b0db2537f89731e255b437c1047
Marian-Claudiu Culea <marianclaudiu.culea@gmail.com>
2021-06-01 16:41:06 +0200

    Update DataLayer.cs

--- BestMovies/Model/Mediator/DataLayer.cs
+++ BestMovies/Model/Mediator/DataLayer.cs
@@ -14,15 +14,21 @@ using BestMovies.Model.Domain;
 
 public interface IDataLayer
 {
-    IList<Movie> AllItems();
-    Movie ItemById(int id);
+    Task<IList<Movie>> AllItems();
+    Task<IList<Movie>> RequestAllItems();
+    Task<IList<Movie>> Top100(string place);
+    Task<IList<Movie>> MostVoted(string place);
+    Task<IList<Movie>> AllUserFavorites();
+    Task<Movie> ItemById(int id);
+    Task<Movie> ItemByName(string name);
+    
+
 }
 
 namespace BestMovies.Pages
 {
     public class DataLayer : IDataLayer
     { 
-
         public IList<Movie> movies { get; set; }
         public static DataLayer instance = null;
         private static readonly object padlock = new object();
@@ -31,8 +37,9 @@ namespace BestMovies.Pages
         {
             client.DefaultRequestHeaders.Accept.Clear();
             client.DefaultRequestHeaders.Accept.Add(
-               new MediaTypeWithQualityHeaderValue("application/vnd.github.v3+json"));
+                new MediaTypeWithQualityHeaderValue("application/json"));
             client.DefaultRequestHeaders.Add("User-Agent", ".NET Foundation Repository Reporter");
+            Console.WriteLine("ready to get");
         }
 
         public static DataLayer Instance
@@ -80,13 +87,55 @@ namespace BestMovies.Pages
         }
 
         */
-        public async Task<IList<Movie>> RequestAllItemsAsync()
+        public async Task<IList<Movie>> RequestAllItems()
         {
-            string uri = "https://localhost:8080/items";
+            string uri = "https://europe-central2-functions-test-314508.cloudfunctions.net/getTop100";
             var streamTask = client.GetAsync(uri);
             var stream = await streamTask.Result.Content.ReadAsStringAsync();
             movies = JsonConvert.DeserializeObject<List<Movie>>(stream);
             return movies;
+        }
+
+        public async Task<IList<Movie>> Top100(string place)
+        {
+            string uri = "https://europe-central2-functions-test-314508.cloudfunctions.net/getTop100?place="+place;
+            var streamTask = client.GetAsync(uri);
+            var stream = await streamTask.Result.Content.ReadAsStringAsync();
+            movies = JsonConvert.DeserializeObject<List<Movie>>(stream);
+            return movies;
+        }
+
+        public async Task<IList<Movie>> MostVoted(string place)
+        {
+            string uri = "https://movies-app-310106.nw.r.appspot.com/api/movies/" + place+ "-votes/";
+            var streamTask = client.GetAsync(uri);
+            var stream = await streamTask.Result.Content.ReadAsStringAsync();
+            movies = JsonConvert.DeserializeObject<List<Movie>>(stream);
+            return movies;
+        }
+
+        public async Task<IList<Movie>> AllUserFavorites()
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task<Movie> ItemByName(string name)
+        {
+            string uri = "https://movies-app-310106.nw.r.appspot.com/api/movies/" + name;
+            var streamTask = client.GetAsync(uri);
+            var stream = await streamTask.Result.Content.ReadAsStringAsync();
+            Movie movie = JsonConvert.DeserializeObject<Movie>(stream);
+            return movie;
+        }
+
+        Task<Movie> IDataLayer.ItemById(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        Task<IList<Movie>> IDataLayer.AllItems()
+        {
+            throw new NotImplementedException();
         }
     }
 }
